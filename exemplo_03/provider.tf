ebd4ecef6d08b3bb69defe761984ff4de1624eb9
Gabriel Miranda de Lima <gabriel.lima@centralit.com.br>
2023-05-22 18:28:01 +0000

    Atualizar 'exemplo_03/provider.tf'
    Include credentials

--- exemplo_03/provider.tf
+++ exemplo_03/provider.tf
@@ -1,6 +1,6 @@
 provider "vsphere" {
-  user           = "<usuário>"
-  password       = "<senha>"
-  vsphere_server = "<endereço IP ou nome do servidor>"
+  user           = "sof.intra\\user_svc_rancher"
+  password       = "#4p0t1@Ranch3r#"
+  vsphere_server = "pvcn01.sof.intra"
   allow_unverified_ssl = true
 }
