9f48edb96c2ec99f9afaae5a7a10bf5cfdfbdd2c
rogeriovs <rogerio.vieira@economia.gov.br>
2024-05-17 15:49:02 -0300

    vars

--- vsphere-sof/sof/envs/lab/ipaddress/vars.tf
+++ vsphere-sof/sof/envs/lab/ipaddress/vars.tf
@@ -1,10 +1,38 @@
-# AWS Credentials
-variable "AWS_ACCESS_KEY" {
-  default = ""
-}
-variable "AWS_SECRET_KEY" {
-  default = ""
-}
-variable "aws_region" {
-  default = "sa-east-1"
-}
+# Virtual Machine configuration
+# datacenter object
+variable "datacenter" {}
+variable "cluster" {}
+variable "datastore" {}
+variable "domain" {}
+variable "netmask" {}
+variable "hostname" {}
+
+# VM Name
+variable "name_prefix" {}
+
+# VM Count
+variable "vm_count" {}
+
+# Name of OVA template (chosen in import process)
+variable "template" {}
+
+# VM Network
+variable "network" {}
+
+# VM Number of CPU's
+variable "cpus" {}
+
+# VM Memory in MB
+variable "memory" {}
+
+variable "vm_pass" {}
+
+variable "ssh_key" {}
+
+variable "vm_user" {}
+
+variable "vm_ip" {}
+
+variable "gateway" {}
+
+variable "dns" {}
