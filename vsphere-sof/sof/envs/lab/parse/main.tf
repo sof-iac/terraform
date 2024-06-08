b42f324841b465e2de6dea393c983e8bce561056
rogeriovs <rogerio.vieira@economia.gov.br>
2024-06-08 18:46:34 -0300

    inline

--- vsphere-sof/sof/envs/lab/parse/main.tf
+++ vsphere-sof/sof/envs/lab/parse/main.tf
@@ -165,35 +165,35 @@ resource "vsphere_virtual_machine" "vm" {
   provisioner "remote-exec" {
     connection {
       type     = "ssh"
-      user     = "${var.vm_user}"
-      password = "${var.vm_pass}"
-      host     = "${var.ipv4_address}"
+      user     = var.vm_user
+      password = var.vm_pass
+      host     = var.ipv4_address
     }
-    command = <<-EOT
-      touch /etc/sudoers.d/ansible_automation
-      echo 'User_Alias ANSIBLE_AUTOMATION = ansible' | tee -a /etc/sudoers.d/ansible_automation
-      echo 'Defaults:ANSIBLE_AUTOMATION !requiretty' | tee -a /etc/sudoers.d/ansible_automation
-      echo 'ANSIBLE_AUTOMATION ALL=(ALL) NOPASSWD: ALL' | tee -a /etc/sudoers.d/ansible_automation
-      chmod 0440 /etc/sudoers.d/ansible_automation
-    EOT
+    inline = [
+      "touch /etc/sudoers.d/ansible_automation",
+      "echo 'User_Alias ANSIBLE_AUTOMATION = ansible' | tee -a /etc/sudoers.d/ansible_automation",
+      "echo 'Defaults:ANSIBLE_AUTOMATION !requiretty' | tee -a /etc/sudoers.d/ansible_automation",
+      "echo 'ANSIBLE_AUTOMATION ALL=(ALL) NOPASSWD: ALL' | tee -a /etc/sudoers.d/ansible_automation",
+      "chmod 0440 /etc/sudoers.d/ansible_automation"
+    ]
   }
   # Quando este recurso é criado, executa o seguinte script localmente para configurar o DNS
   provisioner "remote-exec" {
     connection {
       type     = "ssh"
-      user     = "${var.vm_user}"
-      password = "${var.vm_pass}"
-      host     = "${var.ipv4_address}"
+      user     = var.vm_user
+      password = var.vm_pass
+      host     = var.ipv4_address
     }    
-    command = <<-EOT
-      echo 'options edns0 trust-ad' > /etc/resolv.conf
-      echo 'nameserver 172.27.3.5' >> /etc/resolv.conf
-      echo 'nameserver 172.27.3.6' >> /etc/resolv.conf
-      echo 'nameserver 172.27.3.7' >> /etc/resolv.conf
-      echo 'search sof.intra blocok.sof.remoto' >> /etc/resolv.conf
-      echo 192.168.250.163         PREP02 >> /etc/hosts
-      echo 192.168.250.125         PREP01 >> /etc/hosts
-    EOT
+    inline = [
+      "echo 'options edns0 trust-ad' > /etc/resolv.conf",
+      "echo 'nameserver 172.27.3.5' >> /etc/resolv.conf",
+      "echo 'nameserver 172.27.3.6' >> /etc/resolv.conf",
+      "echo 'nameserver 172.27.3.7' >> /etc/resolv.conf",
+      "echo 'search sof.intra blocok.sof.remoto' >> /etc/resolv.conf",
+      "echo '192.168.250.163         PREP02' >> /etc/hosts",
+      "echo '192.168.250.125         PREP01' >> /etc/hosts"
+    ]
   }  
 }
 
