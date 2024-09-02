bb8705f275b0ba1bc192f232050c6e1e84c701b5
rogeriovs <rogerio.vieira@economia.gov.br>
2024-09-02 20:21:01 -0300

    vmgateway

--- modules/vmware_modules/vm/linux/main.tf
+++ modules/vmware_modules/vm/linux/main.tf
@@ -8,23 +8,23 @@ module "vsphere_linux_vm" {
   vmname    = each.key
   vmtemp    = each.value.template
   instances = each.value.instances
-  template  = each.value.template
-  vsphere_cluster = each.value.vsphere_cluster
+  #template  = each.value.template
+  #vsphere_cluster = each.value.vsphere_cluster
   
   dc                = each.value.datacenter
-  datacenter        = each.value.datacenter
+  #datacenter        = each.value.datacenter
   datastore_cluster = each.value.datastore_cluster
   datastore         = each.value.datastore
   vmrp              = each.value.resource_pool
-  resource_pool     = each.value.resource_pool
+  #resource_pool     = each.value.resource_pool
   
   network     = each.value.network
-  ip_address  = each.value.ip_address
-  ipv4submask = each.value.mask
+  #ip_address  = each.value.ip_address
+  #ipv4submask = each.value.mask
   mask        = each.value.mask
   vmgateway   = each.value.gateway 
-  gateway     = each.value.gateway
-  disk_size   = each.value.disk_size
+  #gateway     = each.value.gateway
+  #disk_size   = each.value.disk_size
   
   # data_disk  = each.value.data-disk
   io_share_level = ["normal", "normal", "normal", "normal", "normal"] #essa linha precisa ser mantida por conta de um bug no módulo
