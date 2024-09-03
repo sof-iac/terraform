802e001d867567fb3266861bfa7471c0720570b1
rogeriovs <rogerio.vieira@economia.gov.br>
2024-09-02 22:09:01 -0300

    disk_size

--- modules/vmware_modules/vm/linux/main.tf
+++ modules/vmware_modules/vm/linux/main.tf
@@ -26,7 +26,7 @@ module "vsphere_linux_vm" {
   #gateway     = each.value.gateway
   #disk_size   = each.value.disk_size
   
-  data_disk  = each.value.disk_size
+  data-disk  = each.value.disk_size
   io_share_level = ["normal", "normal", "normal", "normal", "normal"] #essa linha precisa ser mantida por conta de um bug no módulo
   cpu_number     = each.value.cpu
   ram_size       = each.value.memory
