9bcadc67351ed5f3be9bc88f120cb4809c9d81a9
rogeriovs <rogerio.vieira@economia.gov.br>
2024-09-09 13:38:03 -0300

    category_vcenter

---
+++ modules/vsphere/category_vcenter/terraform_vsphere_category/main.tf
@@ -0,0 +1,10 @@
+    resource "vsphere_tag_category" "category" {
+      name        = var.category_name
+      description = var.category_description
+      cardinality = "MANY"
+      associable_types = ["VirtualMachine"]
+    }
+
+    output "category_id" {
+      value = vsphere_category.category.id
+    }
---
+++ modules/vsphere/category_vcenter/variables.tf
@@ -0,0 +1,8 @@
+variable tag {
+  type = map(object(
+    {
+      category_name        = string
+      category_description = string
+    })
+  )
+}
