11bd5d14a2267de2b210d8a85a9ac7f2f1374a77
rogeriovs <rogerio.vieira@economia.gov.br>
2024-09-09 18:34:40 -0300

    Aspas

--- modules/vsphere/tags_vcenter/terraform_vsphere_tags/variables.tf
+++ modules/vsphere/tags_vcenter/terraform_vsphere_tags/variables.tf
@@ -1,17 +1,17 @@
-    variable "category_name" {
-      description = "Nome da categoria da tag"
-      type        = string
-    }
-    
-    variable "tag_name" {
-      description = "Nome da tag a ser criada"
-      type        = string
-    }
-    
-    variable "tag_description" {
-      description = "Descrição da tag"
-      type        = string
-      default     = ""
-    }
+variable "category_name" {
+  description = "Nome da categoria da tag"
+  type        = string
+}
+
+variable "tag_name" {
+  description = "Nome da tag a ser criada"
+  type        = string
+}
+
+variable "tag_description" {
+  description = "Descrição da tag"
+  type        = string
+  default     = ""
+}
 
 
