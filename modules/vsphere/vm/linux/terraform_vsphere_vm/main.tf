6520f9118b2a6132346af7cafc3a9dd2c41aba2f
rogeriovs <rogerio.vieira@economia.gov.br>
2024-09-13 22:11:23 -0300

    local.os_type

--- modules/vsphere/vm/linux/terraform_vsphere_vm/main.tf
+++ modules/vsphere/vm/linux/terraform_vsphere_vm/main.tf
@@ -1,6 +1,14 @@
 locals {  
   # Extraindo o primeiro IP da variável de rede  
   first_ip = element(flatten([for k, v in var.network : v]), 0)  
+  # Obtendo o sistema operacional correspondente ao primeiro IP  
+  os_type = lookup(var.os_map, local.first_ip, "unknown")  # Retorna "unknown" se o IP não estiver no mapa  
+
+  # Variável para verificar o tipo de SO  
+  is_ubuntu = local.os_type == "ubuntu"  
+  is_centos  = local.os_type == "centos"  
+  is_orcl    = local.os_type == "oracle"  
+  is_windows  = local.os_type == "windows"    
 }
 data "vsphere_datacenter" "dc" {
   name = var.dc
@@ -312,9 +320,9 @@ resource "vsphere_virtual_machine" "vm" {
     } 
     inline = [
       "chmod +x /tmp/setup_ansible_user.sh",
-      "/tmp/setup_ansible_user.sh ${var.svc_password}",
+      "/tmp/setup_ansible_user.sh ${var.local_adminpass}",
       "chmod +x /tmp/config_dns.sh",
-      "/tmp/config_dns.sh ${var.distro}",
+      "/tmp/config_dns.sh ${var.local.os_type}",
     ]
   }  
   # Quando este recurso é criado, executa o seguinte script localmente para dar permissões ao usuario ansible
