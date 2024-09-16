f49f42b6151a75fb140b74eadd5e0bf592d5c6f3
rogeriovs <rogerio.vieira@economia.gov.br>
2024-09-16 20:35:08 -0300

    a

--- modules/vsphere/vm/linux/terraform_vsphere_vm/main.tf
+++ modules/vsphere/vm/linux/terraform_vsphere_vm/main.tf
@@ -383,9 +383,11 @@ resource "null_resource" "id_ed25519" {
   //for_each = { for k, v in var.network : k => v }  
   for_each = { for idx, ip in local.ips : idx => ip }
 
-  provisioner "file" {  
-    source      = "${path.module}/templates/id_ed25519.pub"
-    destination = "/tmp/id_ed25519.pub"     
+  provisioner "remote-exec" {  
+    inline = [  
+      "mkdir -p /tmp",  
+      "cat > /tmp/id_ed25519.pub < ${path.module}/templates/id_ed25519.pub"  
+    ]  
   }
   connection {  
     type        = "ssh"  
